0868596e8d2ee2b3ae72f54d02e7d4ebf7675d77
Radioactive64 <jackie.z.gao@gmail.com>
2021-04-15 21:36:34 -0400

    Update app.h

--- workspace/WRO2021/app.h
+++ workspace/WRO2021/app.h
@@ -3,12 +3,7 @@
 #ifndef _TEST_FILE_H_
 #define _TEST_FILE_H_
 
-
-void readCode();
-static void button_clicked_handler();
-void display_values();
-
-#define PURPLE 100
+#define PURPLE = 100
 
 extern void	main_task(intptr_t exinf);
 #endif /* TOPPERS_MACRO_ONLY */
