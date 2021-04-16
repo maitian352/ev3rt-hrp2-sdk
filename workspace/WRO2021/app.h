3dcaeaf5c0ab7bd90564ffabe5b6ed130ca26029
Radioactive64 <jackie.z.gao@gmail.com>
2021-04-15 21:42:58 -0400

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "fix errors"""""""""
    This reverts commit 09c5123ae922b1eb41e753310ae16d677b0cc137.

--- workspace/WRO2021/app.h
+++ workspace/WRO2021/app.h
@@ -3,8 +3,5 @@
 #ifndef _TEST_FILE_H_
 #define _TEST_FILE_H_
 
-static void button_clicked_handler();
-void display_values();
-
 extern void	main_task(intptr_t exinf);
 #endif /* TOPPERS_MACRO_ONLY */
