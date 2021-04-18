10380b787151926f681d33fd95164df151688dcd
Radioactive64 <jackie.z.gao@gmail.com>
2021-04-17 21:17:00 -0400

    Update app.h

--- workspace/WRO2021/app.h
+++ workspace/WRO2021/app.h
@@ -3,17 +3,22 @@
 #ifndef _TEST_FILE_H_
 #define _TEST_FILE_H_
 
+#define NONE 0
+#define RED 1
+#define GREEN 2
+#define BLUE 3
+#define WALL 4
+#define LEFT 1
+#define RIGHT 4
+
+void init();
+void open_carbay(int door);
+void close_carbay(int door);
+void readcar(int sansar);
 void display_values();
 void linePID(int distance);
+void drive(int distance, int curve);
 void button_clicked_handler(intptr_t button);
-
-#define NONE = 0
-#define RED = 1
-#define GREEN = 2
-#define BLUE = 3
-#define WALL = 4
-#define LEFT = 1
-#define RIGHT = 4
 
 extern void	main_task(intptr_t exinf);
 #endif /* TOPPERS_MACRO_ONLY */
