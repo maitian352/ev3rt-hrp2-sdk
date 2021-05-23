57f6dbc53e7b41ba248380a73978ef5394e26d16
Radioactive64 <jackie.z.gao@gmail.com>
2021-05-22 21:43:32 -0400

    fix agian

--- workspace/WRO2021/app.c
+++ workspace/WRO2021/app.c
@@ -327,7 +327,7 @@ void PID(int distance, int power, int turn, int turn_sensor, int readCar) {
 
 void openDoor(int car, int location) {
     ev3_motor_rotate(a_motor, (doorLocations[car][location][0]-ev3_motor_get_counts(a_motor)), 20, false);
-    ev3_motor_rotate(d_motor, (doorLocations[car][location][1]-ev3_motor_get_counts(d_motor), 20, true));
+    ev3_motor_rotate(d_motor, (doorLocations[car][location][1]-ev3_motor_get_counts(d_motor)), 20, true);
 }
 void closeDoor() {
     ev3_motor_rotate(a_motor, (-ev3_motor_get_counts(a_motor)), 20, false);
