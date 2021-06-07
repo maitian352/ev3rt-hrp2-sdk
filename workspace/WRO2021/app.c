181fae0047cb6938f109e7621b83f4680178f695
Radioactive64 <jackie.z.gao@gmail.com>
2021-06-06 20:12:18 -0400

    what why this exist

--- workspace/WRO2021/app.c
+++ workspace/WRO2021/app.c
@@ -7,14 +7,6 @@
 #include <string.h>
 #include <math.h>
 
-#define DEBUG
-
-#ifdef DEBUG
-#define _debug(x) (x)
-#else
-#define _debug(x)
-#endif
-
 // define motors and sensors
 const int color_sensor1 = EV3_PORT_1, color_sensor2 = EV3_PORT_2, color_sensor3 = EV3_PORT_3, color_sensor4 = EV3_PORT_4, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C, a_motor = EV3_PORT_A, d_motor = EV3_PORT_D;
 
